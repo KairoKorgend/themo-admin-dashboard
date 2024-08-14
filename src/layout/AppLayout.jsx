a4f26c328634adaf8f34228ef32735909313dbc7
KairoKorgend <korgendkairo@gmail.com>
2024-08-14 17:27:53 +0300

    added background image

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -15,7 +15,7 @@ const DefaultLayout = () => {
         <div className="body flex-grow-1">
           <AppContent />
         </div>
-        <AppFooter />
+        {/* <AppFooter /> */}
       </div>
     </div>
   );
